793e8f0378d1e796e31d13b69d17f76d585772e9
mlch911 <mlch911@mluoc.tech>
2017-05-29 03:30:58 +0800

    Ver 0.5

---
+++ FansArea/MapViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MapViewController.swift
+//  FansArea
+//
+//  Created by 闵罗琛 on 2017/5/25.
+//  Copyright © 2017年 闵罗琛. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FansArea/ReviewViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ReviewViewController.swift
+//  FansArea
+//
+//  Created by 闵罗琛 on 2017/5/23.
+//  Copyright © 2017年 闵罗琛. All rights reserved.
+//
+
+import UIKit
+
+class ReviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
